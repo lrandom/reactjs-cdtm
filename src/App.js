8707e28697e2af7591c6610caee614575f651101
Nguyen Thanh Luan <luann4099@gmail.com>
2021-12-24 09:37:31 +0700

    update demo session-3

--- src/App.js
+++ src/App.js
@@ -1,52 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav.js';
-import Ex1 from './components/Ex1.js';
-
-import {name} from './demo/helper.js';
-import {getTotal} from "./demo/helper.js";
-import {obj} from './demo/helper.js';
-import getMyName from './demo/helper';
+import MyComponent1 from "./components/MyComponent1";
+import MyComponent2 from "./components/MyComponent2";
+import {useState} from "react";
 
 function App() {
-    const schoolName = "Cao đẳng công nghệ và thương mại";
-    const getTotal = (a, b) => {
-        return a + b;
+    const [isShow, setShow] = useState(true);
+    const deleteComponent2 = () => {
+        setShow(false);
     }
-    const imgSrc = 'https://image.winudf.com/v2/image1/Y29tLm5hdHVyYWxpbWFnZWhkd2FsbHBhcGVyLnJ5cnNfc2NyZWVuXzBfMTU0MjM3MDgxNl8wODU/screen-0.jpg?fakeurl=1&type=.jpg';
-    const obj = {
-        backgroundColor: 'red'
-    }
-    const clickMe = () => {
-        alert('Click me');
-    }
-    const isRenderA = false;
-    let list = [
-        10,
-        20,
-        30,
-        40
-    ];
     return (
-        <div className="App">
-            {
-                isRenderA ? <Nav/> : <div>Hello</div>
-            }
-            {schoolName}
-            {10 * 20}
-            {getTotal(10, 20)}
-            <img className="img" style={obj} src={imgSrc}/>
-            <button onClick={clickMe}>Click me, bro</button>
-            {
-                list.map((item, index) => {
-                    return <div key={index}>{item}</div>
-                })
-            }
-            <Ex1></Ex1>
-            <div>{name}</div>
-            <div>{getTotal(10, 10)}</div>
-            <div>{obj.name}</div>
-            <div>{getMyName}</div>
+        <div>
+            {/*  <MyComponent1></MyComponent1>*/}
+            {isShow && <MyComponent2/>}
+            <button onClick={() => {
+                deleteComponent2()
+            }}>Delete MyComponent 2
+            </button>
         </div>
     );
 }
--- src/components/MyComponent2.js
+++ src/components/MyComponent2.js
@@ -3,13 +3,50 @@ import React from "react";
 class MyComponent2 extends React.Component {
     constructor() {
         super();
+        this.state = {
+            isShowDiv: false,
+            isShowSpan: false,
+            name: 'Luan'
+        }
+        console.log("init Component");
+
+
+        //this.setState({isShowDiv: true});
+        setTimeout(() => {
+            this.setState({isShowDiv: true, name: 'Nguyen Thanh Luan'});
+        }, 3000);
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log("componentWillUpdate");
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("componentDidUpdate");
+    }
+
+    componentWillMount() {
+        console.log("Component Will Mount");
+    }
+
+    componentDidMount() {
+        console.log("Component Did Mount");
     }
 
     //đây là phuơng thức được gọi để tạo giao diện
     render() {
         return (
             <div>
-                <h1>MyComponent 2</h1>
+                This is component 2
             </div>
         );
     }
